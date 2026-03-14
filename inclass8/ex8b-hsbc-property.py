ac7ac1bd7da252621cf40d6bce287b87cd7b628d
spagestic <steelmax666@gmail.com>
2026-03-14 14:05:01 +0800

    inclass 8

---
+++ inclass8/ex8b-hsbc-property.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+# Complete the following function to get HSBC property valuation
+# 
+# Website: https://www.hsbc.com.hk/zh-hk/personal/products/mortgage/property-valuation
+#
+# Use it to find the valuation of
+# - Flat A, Block/Tower 1,Sorrento,Tsimshatsui,Kowloon of 8 - 15/F
+#
+
+def get_hsbc_valuation(zoneId, districtId, estateId, blockId, floor, flat):
+    # TODO write your code here
+    return 
+
